3525dcb6be5a5751f2dfea2168f8bc9c0e22aa89
MariiaMarchenko <96719405+MariiaMarchenko@users.noreply.github.com>
2023-11-26 19:11:38 +0100

    sort

--- src/components/Products/ProductPage.jsx
+++ src/components/Products/ProductPage.jsx
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useProducts } from '../../hooks/useProduct';
 
 const ProductPage = () => {
     const { products } = useProducts();
+    const [sortedProducts, setSortedProducts] = useState([...products]);
+    const [sortType, setSortType] = useState('');
+
+    const handleSortChange = (value) => {
+        setSortType(value);
+        sortProducts(value);
+    };
+
+    const sortProducts = (type) => {
+        let sorted;
+
+        if (type === 'price-asc') {
+            sorted = [...products].sort((a, b) => a.price - b.price);
+        } else if (type === 'price-desc') {
+            sorted = [...products].sort((a, b) => b.price - a.price);
+        } else if (type === 'alphabetical') {
+            sorted = [...products].sort((a, b) => a.name.localeCompare(b.name));
+        } else {
+            sorted = [...products];
+        }
+
+        setSortedProducts(sorted);
+    };
 
     return (
         <div className="products">
             <p className="shopTitle">Товари для тварин</p>
+            <div className={"selector"}>
+                <select value={sortType} onChange={(e) => handleSortChange(e.target.value)}>
+                    <option value="">Сортувати за:</option>
+                    <option value="price-asc">Ціною (зростаюча)</option>
+                    <option value="price-desc">Ціною (спадаюча)</option>
+                    <option value="alphabetical">За абеткою</option>
+                </select>
+            </div>
+
+
+
             <div className="product-container">
-                {products.map((product) => (
+                {sortedProducts.map((product) => (
                     <div key={product.id} className="product">
                         <Link to={`/product/${product.id}`} className="product-link">
                             <img
@@ -20,6 +54,9 @@ const ProductPage = () => {
                             <div className="description">
                                 <p>
                                     <b>{product.name}</b>
+                                </p>
+                                <p>
+                                    <b>{product.price}</b><b>грн</b>
                                 </p>
                             </div>
                         </Link>
